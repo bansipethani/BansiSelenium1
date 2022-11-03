bbab78906125112360ac7758626b9f47a0723d9b
bansipethani <bansirpethani@gmail.com>
2022-11-03 13:54:21 -0600

    FirstTry

--- src/test/java/com/ict/learning/bansi11111/DemoSele.java
+++ src/test/java/com/ict/learning/bansi11111/DemoSele.java
@@ -1,8 +1,17 @@
 package com.ict.learning.bansi11111;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoSele {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\bansi\\eclipse-workspace-new\\LearnSel\\lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://tigernet.campbellsville.edu/ics/");
+		driver.manage().window().fullscreen();
+		String title = driver.getTitle();
+		System.out.println(title);
 		
 
 	}
